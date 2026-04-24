e0690e6e1f210ddd0f5588477039f43743193e00
Masterprem1 <169062143+Masterprem1@users.noreply.github.com>
2026-04-24 21:27:15 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+a = int(input("Give me the first number: "))
+b = int(input("Give me the second number: "))
+print (f'Thank you!')
+print (f"{a} + {b} = {a+b}")
+print (f"{a} - {b} = {a-b}")
+print (f"{a} / {b} = {a/b:.0f}")
+print (f"{a} * {b} = {a*b}")
