55a18627532cd60d594ca621bc19c57f4794d8d8
Masterprem1 <169062143+Masterprem1@users.noreply.github.com>
2026-04-24 17:01:34 +0700

    Add files via upload

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,7 @@
+while True :
+    a = 0
+    for i in range(11):
+        print (f"Table de {a}: {a*0} {a*1} {a*2} {a*3} {a*4} {a*5} {a*6} {a*7} {a*8} {a*9} {a*10}")
+        a +=1 
+    if a == 11 :
+        break
