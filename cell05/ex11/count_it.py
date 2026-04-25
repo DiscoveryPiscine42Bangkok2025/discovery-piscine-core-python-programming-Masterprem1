2a95b3635f24c3178c2c3f368685d9bf4a8ea01d
Masterprem1 <169062143+Masterprem1@users.noreply.github.com>
2026-04-25 12:47:52 +0700

    Add files via upload

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,10 @@
+a = input()
+raw_word = a.split()
+words = []
+for item in raw_word :
+    clean = item.strip('"')
+    words.append(clean)
+
+print (f'parameters : {len(words)}')
+for word in words :
+    print (f'{word} : {len(word)}')
